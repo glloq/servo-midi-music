393bd5283b173d13232ec43fe91f31c82caca5e6
glloq <glloq.nz@gmail.com>
2024-10-04 17:55:16 +0200

    Update ServoController.h

--- ServoOnOff/ServoController.h
+++ ServoOnOff/ServoController.h
@@ -15,7 +15,7 @@ class ServoController {
 private:
   Adafruit_PWMServoDriver pwm[MAX_NUM_PCA9685];  // Utilisation d'un tableau pour stocker les objets PCA9685
   void setServoAngle(uint8_t servoNum, uint16_t angle);
-  void checkPowerOn();// viens verifer que la pin OE est a 0 pout faire un mouvment d'un servo
+  void checkPowerOn();// viens verifer que la pin OE est a 0 pour faire un mouvment d'un servo
   bool isUnpowered; // stocke l'etat du pin OE 
   unsigned long TimeToUnpower ; //temps quand il faut desactiver les servo
 public:
